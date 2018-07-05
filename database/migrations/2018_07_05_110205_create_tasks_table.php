a6138f5a7c84876043448586dd2692927a1783b8
Akul Goel <akulgoel@MacBook-Pro-109.local>
2018-07-05 17:18:32 +0530

    Reinitialised files

---
+++ database/migrations/2018_07_05_110205_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('task_desc');
+            $table->unsignedInteger('list_id');
+            $table->timestamps();
+            $table->foreign('list_id')->references('id')->on('lists');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
